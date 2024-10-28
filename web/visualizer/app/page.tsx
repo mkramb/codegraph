240edbe7c5f5c002bdfb37daa7b19daafe59116d
Mitja Kramberger <mitja.kramberger@gmail.com>
2024-10-28 21:44:32 +0000

    Initial setup for visualization app

--- web/visualizer/app/page.tsx
+++ web/visualizer/app/page.tsx
@@ -1,7 +1,30 @@
-export default function Home() {
+"use client"
+
+import { useEffect, useState } from "react"
+import { GraphComponent } from "./components/graph"
+
+export default function App() {
+  const [data, setData] = useState(null)
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch("http://localhost:3000/data.json")
+      const result = await response.json()
+
+      setData(result)
+      setLoading(false)
+    }
+
+    fetchData()
+  }, [])
+
+  if (loading) return <div>Loading...</div>
+  if (!data) return <div>Error loading data.</div>
+
   return (
-    <div>
-      <h1>Visualizer</h1>
+    <div className="p-20">
+      <GraphComponent />
     </div>
-  );
+  )
 }
